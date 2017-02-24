da0c9d8b0bc2b60eec6fb19941d77c029ecd3e6f
John P Considine <jconsidi@nd.edu>
2017-02-23 23:22:23 -0500

    Processed

--- next.php
+++ next.php
@@ -6,6 +6,6 @@
 #exec("python scripts/sendemail.py jconsidi@nd.edu");
 
 for ($x = 0; $x <= intval($_POST['numberBouts']); $x++) {
-    echo "The number is: $x <br>";
+    echo $_POST['bout' . $x]
 }  
 ?>
