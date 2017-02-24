7d262dd7051e5d169359a2a7c0821251e969ff56
John P Considine <jconsidi@nd.edu>
2017-02-24 11:38:25 -0500

    Processed

--- js/checkform.js
+++ js/checkform.js
@@ -16,7 +16,7 @@ $('.submit-pickem').click(function () {
 			console.log($("input[name=bout" + (i+1) + "]:checked").val());
 		}
 		json_string +="]";
-		alert(JSON.parse(json_string)["bout1"]);
+		alert(json_string);
 
 		 // $.ajax({
    //         type: "POST",
