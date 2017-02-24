8b9366e7fce8c2cc6e422520f096d8358caa43f4
John P Considine <jconsidi@nd.edu>
2017-02-24 14:22:14 -0500

    Processed

--- next.php
+++ next.php
@@ -13,8 +13,8 @@ $message = $message . "And your verification code is: \n" . generateRandomString
 // $email = $_POST['email'];
 
 
-$data = json_decode(file_get_contents('php://input'));
-var_dump($data->name);
+var_dump(json_decode(file_get_contents("php://input")));
+
 
 
 function generateRandomString($length = 10) {
