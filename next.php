1e7585a5d7c8f7318e71f706c3a8eed3942fac0b
John P Considine <jconsidi@nd.edu>
2017-02-23 23:46:31 -0500

    Processed

--- next.php
+++ next.php
@@ -6,7 +6,7 @@
 #exec("python scripts/sendemail.py jconsidi@nd.edu");
 $message = "Here are your pix from the bb matchups\n";
 for ($x = 0; $x <= intval($_POST['numberBouts']); $x++) {
-    echo $x;
+    echo $_POST['bout'. $x];
 }  
 
 
