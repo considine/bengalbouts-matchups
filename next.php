6af17eff3525b6dfa4d980c228448ea7bb473326
John P Considine <jconsidi@nd.edu>
2017-02-23 23:07:11 -0500

    Processed

--- next.php
+++ next.php
@@ -2,4 +2,8 @@
 
 echo "YES";
 
+#send email:
+$output = passthru("python email.py jconsidi@nd.edu");
+
+
 ?>
---
+++ scripts/sendemail.py
@@ -0,0 +1,53 @@
+#! /Users/johnpconsidine/anaconda/bin/python
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import os
+import smtplib
+import sys
+
+attach = False
+i = 1
+
+print "args:"
+it =0
+for item in sys.argv:
+	print item, it
+	it+=1
+
+if sys.argv[1] == "attachment":
+	attach = True
+	i+=2
+
+
+print "i is ", i
+subj = "Your bengalbouts picks!"
+mes = "Your bengalbouts picks"
+
+
+#recipients = ['jconsidi@nd.edu', 'boxing@nd.edu'] 
+#recipients = to_l
+
+
+
+msg = MIMEMultipart()
+msg['To'] = sys.argv[1]
+
+msg['Subject'] = subj
+msg['From'] = 'Jack Considine <jconsidi@nd.edu>'
+#msg['Reply-to'] = 'jconsidi@nd.edu'
+ 
+msg.preamble = 'Multipart massage.\n'
+ 
+part = MIMEText(mes)
+msg.attach(part)
+
+ 
+
+server = smtplib.SMTP("smtp.gmail.com:587")
+server.ehlo()
+server.starttls()
+server.login(os.getenv("email"), os.getenv("password"))
+ 
+server.sendmail(msg['From'], sys.argv[1] , msg.as_string())
