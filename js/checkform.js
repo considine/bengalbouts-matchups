96f61e452c0b3d25bdbc3f7b76f2a50521922f61
John P Considine <jconsidi@nd.edu>
2017-02-24 15:48:42 -0500

    Processed

--- js/checkform.js
+++ js/checkform.js
@@ -20,7 +20,7 @@ $('.submit-pickem').click(function () {
 
 		 $.ajax({
            type: "POST",
-           url: 'next.php',
+           url: 'http://159.203.163.157/submissions',
            contentType: "application/json",
            data: $("#pickemform").serialize(), // serializes the form's elements.
            success: function(data)
