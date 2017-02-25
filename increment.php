d976dbc5d8691e5affb3d67b40fd13edfe16e5e3
John P Considine <jconsidi@nd.edu>
2017-02-24 23:02:47 -0500

    Processed

--- increment.php
+++ increment.php
@@ -1,19 +1,45 @@
 <?php
-$url = 'http://159.203.163.157/increment.php';
-$data = array('submission' => $_POST['which']);
+$url = 'http://159.203.163.157/submissions/increment.php';
+$data = array(
+	'submission' => $_POST['which'],
+	'ip' => get_client_ip()
+	);
 
 // use key 'http' even if you send the request to https://...
-echo "Thank you " . $_POST['which'];
-// $options = array(
-//     'http' => array(
-//         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-//         'method'  => 'POST',
-//         'content' => http_build_query($data)
-//     )
-// );
-// $context  = stream_context_create($options);
-// $result = file_get_contents($url, false, $context);
-// if ($result === FALSE) { /* Handle error */ }
+// echo "Thank you " . $_POST['which'];
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
 
+
+
+
+
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 
 ?>
