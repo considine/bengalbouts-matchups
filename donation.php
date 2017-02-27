a577bcfe7808be1c1923d7cf6953264e13f3b2c8
John P Considine <jconsidi@nd.edu>
2017-02-26 23:53:19 -0500

    Processed

--- donation.php
+++ donation.php
@@ -1,7 +1,20 @@
-<?php
-header('Location: https://app.mobilecause.com/f/18dv/n');
+
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+  
+  </body>
+  <script type="text/JavaScript" src="js/checkform.js"> </script>
+  <script> 
+  	
+  	increment("donation");
+  	window.location = "https://app.mobilecause.com/f/18dv/n";
+  </script>
+</html>
 
 
 
-
-?>
