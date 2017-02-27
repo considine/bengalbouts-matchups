2e6c8ec834a7475839e9714499ccadc824f5f871
John P Considine <jconsidi@nd.edu>
2017-02-26 23:53:58 -0500

    Processed

--- donation.php
+++ donation.php
@@ -8,6 +8,9 @@
   <body>
   
   </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
   <script type="text/JavaScript" src="js/checkform.js"> </script>
   <script> 
   	
